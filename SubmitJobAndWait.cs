6a2643ea34fb00c8186bdfc2640fded25957b54a
DanielSMVPSI <DanielS@MVPSi.com>
2017-04-20 08:39:36 -0400

    Update and rename SubmitJobAndWait to SubmitJobAndWait.cs

--- SubmitJobAndWait.cs
+++ SubmitJobAndWait.cs
@@ -1,4 +1,23 @@
 //
+// Get a connection to our JAMS Server
+// You will probably have to change the node name "localhost"
+// to the name of your JAMS Server
+//
+JAMS.Server server = JAMS.Server.GetServer("localhost");
+//
+// Load the job that we want to submit
+//
+JAMS.Submit.Info si;
+JAMS.Submit.Load(out si, "BackupDB", server, JAMS.Submit.Type.Job);
+//
+// Finish the submit
+//
+si.Submit()
+
+// Monitor a Job After Submitting
+
+// If you want the application to wait for the completion  and then check you could add this code after your code:
+//
             // Wait for the entry to complete with Info for better
             //
             WaitFor waitFor = new WaitFor(server);
