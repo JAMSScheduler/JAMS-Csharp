a42c3873e9ee0bd18132705bef9230335d70c24c
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2022-06-01 15:24:09 -0400

    Delete QueryHistory.cs
    Removing old code from current branch

--- QueryHistory.cs
+++
@@ -1,8 +0,0 @@
-
- // Get the JAMS Server
- Server svr = Server.GetServer("localhost");
- // Define search criteria
- List<HistorySelection> criteria = new List<HistorySelection>();
- criteria.Add(new HistorySelection(HistorySelectionField.JobName, ComparisonOperator.Like, "CRM*"));
- // Query History
- ICollection<History> historyResult = History.Find(criteria, DateTime.Today, DateTime.Now, true, true, true, true, true, true, true, true, true, HistorySearchOptions.None, svr);
