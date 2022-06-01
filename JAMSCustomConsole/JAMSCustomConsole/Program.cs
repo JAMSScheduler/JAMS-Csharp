1fa0d7e71fb0a6f30d14a774c8e9a21204620c14
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2022-06-01 15:33:41 -0400

    V7 projects to submit job
    Adding new V7 code projects for submitting jobs

---
+++ JAMSCustomConsole/JAMSCustomConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using MVPSI.JAMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMSCustomConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = Server.GetServer("DF-JAMS7.helpsysdev.com");
+
+            Submit.Info si;
+            Submit.Load(out si, "\\Samples\\Sleep180", server);
+           
+            si.Submit();
+            
+
+        }
+    }
+}
---
+++ JAMSCustomUI/JAMSCustomUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using MVPSI.JAMS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAMSCustomUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var server = Server.GetServer("DF-JAMS7.helpsysdev.com");
+
+            Submit.Info si;
+            Submit.Load(out si, textBox1.Text, server);
+            si.Submit();
+        }
+    }
+}
