fb9ab1e485d65ff5c15f4fd7431239016583906f
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2023-05-31 10:58:12 -0400

    Added Custom Workflow activity solution
    Added C# project to create new workflow activity that gets the last modified datetime of a specified File.  The resulting libraries can be used to add new activity to workflow toolbox.

---
+++ CustomActivityLib/CustomActivityLib/GetFileLastModifiedDate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MVPSI.JAMS.Activities;
+using MVPSI.JAMS;
+using System.IO;
+using MVPSI.JAMS.Extensions;
+
+namespace CustomActivityLib
+{
+    public sealed class GetFileLastModifiedDate : NativeActivity
+    {
+        public GetFileLastModifiedDate()
+        {
+        }
+
+        // Define an activity input argument of type string
+        public InArgument<string> FilePath { get; set; }
+        public OutArgument<string> LastModifiedDate { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(NativeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            string path = context.GetValue(this.FilePath);
+
+            FileInfo file = new FileInfo(path);
+            // last write time
+            DateTime dt = file.LastWriteTime;
+            string dateString = dt.ToString("G");
+            this.LastModifiedDate.Set(context, dateString);
+
+
+        }
+    }
+}
